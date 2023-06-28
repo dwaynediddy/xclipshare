88ea007738c236cf53aa243d840137d1e01aca4a
dwaynediddy <burrows.dwayne@icloud.com>
2023-06-28 12:03:37 +0200

    attempting to add an author

--- src/components/posts/postsSlice.js
+++ src/components/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
     {id: 1, title: "getting this going", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"},
@@ -9,10 +9,21 @@ const postsSlice = createSlice({
     name: 'post',
     initialState,
     reducers: {
-        postAdded(state, action) {
-            state.push(action.payload)
+        postAdded: {
+            reducer(state, action) {
+                state.push(action.payload)
+            },
+            prepare(title, content, userId) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title: title,
+                        content: content,
+                        userId
+                    }
+                }
+            }
         }
-
     }
 })
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './style/home.css'
 // import Clip from '../components/Clip'
 import PostsList from '../components/PostsList'
-import AddPostForm from '../components/AddPostForm'
+import AddPostForm from '../components/posts/AddPostForm'
 
 const Home = () => {
   return (
